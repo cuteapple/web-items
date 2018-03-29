c5fa9a94adce1f75d2a95ed69e549e327c7ea5b6
cute apple <hidden>
2018-03-29 23:04:56 +0800

    init

---
+++ 8-puzzle/8-puzzle.js
@@ -0,0 +1,25 @@
+let playground;
+let nodes;
+
+function hint(text) {
+    document.getElementById('hint').innerText = text;
+}
+
+function init() {
+    playground = document.getElementById('playground')
+    nodes = [...playground.querySelectorAll('div')]
+
+    "TODO: init nodes"
+}
+
+function clicknode(node) {
+    "TODO: try move"
+}
+
+function keydown() {
+    "TODO: try move"
+}
+
+function try_finish() {
+    "TODO: check order(?) or each block check itself(?) custom element "
+}
