df2cf8cdb96628f511606b626ee0f8619da9a7c9
cute apple <hidden>
2018-04-11 17:51:45 +0800

    inheritance from PlaygroundItem

--- snake/snake.js
+++ snake/snake.js
@@ -10,6 +10,63 @@ function init() {
     controller.all = movekey_handler;
 }
 
+function move([dx, dy]) {
+    let [x, y] = body[0].pos
+    x += dx
+    y += dy
+
+    let status;
+
+    //wall
+    if (x <= 0 || x > 50 || y <= 0 || y > 50) { end('❌'); return; }
+
+    //food
+    if (food) { }
+
+
+    for (let i = body.length - 1; i > 0; --i) {
+        body[i].pos = body[i - 1].pos
+    }
+
+    body[0].x += dx;
+    body[0].y += dy;
+}
+
+class PlaygroundItem {
+    constructor(x, y) {
+        this.element = document.createElement('div')
+        this.x = x
+        this.y = y
+        playground.appendChild(this.element)
+    }
+    set x(value) { this.element.style.gridColumn = this._x = value; }
+    get x() { return this._x; }
+    set y(value) { this.element.style.gridRow = this._y = value; }
+    get y() { return this._y; }
+    set pos(p) { this.x = p[0]; this.y = p[1]; }
+    get pos() { return [this.x, this.y] }
+}
+
+class SnakeBody extends PlaygroundItem{
+    constructor(x, y, head = false) {
+        super(x,y)
+        this.element.classList.add("snake")
+        if (head) this.element.classList.add("head")
+        this.head = head
+    }
+}
+
+class Food extends PlaygroundItem {
+    moveToEmpty() {
+        //...
+        items = [food,...body]
+    }
+}
+
+function hint(text) {
+    document.getElementById('hint').innerText = text;
+}
+
 function end(message = "end") {
     controller.all = () => { }
     hint(message)
@@ -18,46 +75,4 @@ function end(message = "end") {
 function movekey_handler(direction) {
     velocity = controller4.to2D(direction, [1, -1])
     move(velocity)
-}
-
-function move([dx, dy]) {
-    let [x, y] = body[0].pos
-    x += dx
-    y += dy
-
-    let status;
-    if (x <= 0 || x > 50 || y <= 0 || y > 50) { end('❌'); return; }
-
-
-
-    for (let i = body.length - 1; i > 0 ; --i) {
-        body[i].pos = body[i-1].pos
-    }
-
-    body[0].x += dx;
-    body[0].y += dy;
-}
-
-function hint(text) {
-    document.getElementById('hint').innerText = text;
-}
-
-class SnakeBody {
-    constructor(x, y, head = false) {
-        this.element = document.createElement('div')
-        this.element.className = "snake"
-        if (head) this.element.classList.add("head")
-        playground.appendChild(this.element)
-
-        this.head = head
-        this.x = x
-        this.y = y
-    }
-
-    set x(value) { this.element.style.gridColumn = this._x = value; }
-    get x() { return this._x; }
-    set y(value) { this.element.style.gridRow = this._y = value; }
-    get y() { return this._y; }
-    set pos(p) { this.x = p[0]; this.y = p[1]; }
-    get pos() { return [this.x, this.y] }
 }
